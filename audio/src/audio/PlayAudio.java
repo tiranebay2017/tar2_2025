eb391010383b0c213b4d5feefc8fcbd22ea36abf
Tiran1990 <t.hesawi1990@gmail.com>
2024-11-16 22:02:33 +0200

    new start

---
+++ audio/src/audio/PlayAudio.java
@@ -0,0 +1,37 @@
+package audio;
+
+import javax.sound.sampled.*;
+import java.io.*;
+
+public class PlayAudio {
+
+    public static void main(String[] args) {
+
+        File audioFile = new File("audio1.wav");
+
+        try {
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+
+            AudioFormat format = audioStream.getFormat();
+
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+
+            Clip audioClip = (Clip) AudioSystem.getLine(info);
+
+            audioClip.open(audioStream);
+
+            audioClip.start();
+
+            while (!audioClip.isRunning())
+                Thread.sleep(10);
+            while (audioClip.isRunning())
+                Thread.sleep(10);
+
+            audioClip.close();
+
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException | InterruptedException e) {
+            System.out.println("Error playing audio file.");
+            e.printStackTrace();
+        }
+    }
+}
